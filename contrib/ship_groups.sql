810f47fb929696af07aafa64b787583203a753b3
Alexander Schittler <me@damongant.de>
2016-02-11 11:28:02 +0100

    contrib stuffs

---
+++ contrib/ship_groups.sql
@@ -0,0 +1,3 @@
+﻿SELECT "invGroups"."groupID", "invGroups"."groupName" FROM "invGroups"
+LEFT JOIN "invCategories" ON ("invGroups"."categoryID" = "invCategories"."categoryID")
+WHERE "invCategories"."categoryName" = 'Ship' 
---
+++ contrib/ship_types.sql
@@ -0,0 +1,4 @@
+﻿SELECT "invTypes"."typeID", "invGroups"."groupID", "invGroups"."groupName", "invTypes"."typeName", "invTypes"."description" FROM "invTypes"
+LEFT JOIN "invGroups" ON ("invGroups"."groupID" = "invTypes"."groupID")
+LEFT JOIN "invCategories" ON ("invGroups"."categoryID" = "invCategories"."categoryID")
+WHERE "invCategories"."categoryName" = 'Ship' 
