29e3a8523c2631af6243618db4768a149f1e0d15
Alexander Schittler <me@damongant.de>
2016-04-08 10:50:33 +0200

    @a

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 "use strict";
 
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get("/", function(req, res, next) {
 	res.status(200).send({"pkg": "osssrp"});
 })
+
+app.use(require("./api"));
 
 module.exports = app;
--- server/config/pgsql.js
+++ server/config/pgsql.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const config = require("./env");
-const pg = require('pg')
+const pg = require('pg');
 
 module.exports = function(injector) {
 	var client = new pg.Client(config.pg);
