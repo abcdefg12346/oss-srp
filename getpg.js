49a39cd6bfd9c28b08d4b0d93e0a3c6183003d42
skr0tm <skr0tm@protonmail.com>
2016-04-13 10:08:20 +0200

    still not much of a clue

--- getpg.js
+++ getpg.js
@@ -1,25 +1,78 @@
 
-  var pgp = require('pg-promise')(/*options*/);
+var express = require('express');
 
-  var cn = {
-    host: 'localhost',
-    port: 5432,
-    database: 'oss-srp',
-    user: 'eve',
-    password: 'oss'
-  };
+var connectionString = "postgres://eve:oss@localhost:5432/oss-srp";
 
-  var db = pgp(cn); // database instance;
+var router = express.Router();
+var pg = require('pg');
+// create
+router.post('/api/v1/todos', function(req, res) {
+
+    var results = [];
+
+    // Grab data from http request
+    var data = {text: req.body.text, complete: false};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Insert Data
+        client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
 
 
-function query(sql){
-  db.query(sql)
-    .then(function (user) {
-        console.log('found data');
-    })
-    .catch(function (error) {
-        console.log(error);
     });
-}
+});
+//read
+router.get('/api/v1/todos', function(req, res) {
+
+    var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+    });
+
+});
+
 
 
