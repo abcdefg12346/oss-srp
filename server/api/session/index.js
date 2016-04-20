7fd64c9bb4a8336b1f7fb1662564c094569f5822
Alexander Schittler <me@damongant.de>
2016-04-20 11:37:21 +0200

    Whoop de doo

--- server/api/session/index.js
+++ server/api/session/index.js
@@ -1,28 +1,30 @@
-const express = require("express");
+'use strict'
+
+const express = require('express');
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const querystring = require("querystring");
-const request = require("request-promise");
-const config = require("../../config");
+const jwt = require('jsonwebtoken');
+const querystring = require('querystring');
+const request = require('request-promise');
+const config = require('../../config');
 
 // get the EVE Online redirect URI
-router.get("/", function(req, res) {
-	const endpoint = "https://login.eveonline.com/oauth/authorize";
+router.get('/', function(req, res) {
+	const endpoint = 'https://login.eveonline.com/oauth/authorize';
 	const params = {
-		response_type: "code",
-		redirect_uri: config.host + "/session/crest/",
+		response_type: 'code',
+		redirect_uri: config.host + '/session/crest/',
 		client_id: config.crest.client_id,
-		scope: "",
+		scope: '',
 		state: req.query.scope
 	}
-	const url = endpoint + "?" + querystring.stringify(params);
+	const url = endpoint + '?' + querystring.stringify(params, null, null, (q) => return q);
 	res.status(200).send({authorize: url});
 })
 
-router.get("/crest", function(req, res, next) {
-	const endpoint = "https://login.eveonline.com/oauth/token";
+router.get('/crest', function(req, res, next) {
+	const endpoint = 'https://login.eveonline.com/oauth/token';
 	const params = {
-		grant_type: "authorization_code",
+		grant_type: 'authorization_code',
 		code: req.query.code
 	};
 	const auth = {
@@ -32,14 +34,14 @@ router.get("/crest", function(req, res, next) {
 
 	request.post(endpoint, {form: params, auth: auth}).then(function(body) {
 		const tokenData = JSON.parse(body);
-		const endpoint = "https://login.eveonline.com/oauth/verify";
+		const endpoint = 'https://login.eveonline.com/oauth/verify';
 		const headers = {
-			"Authorization": "Bearer " + tokenData.access_token
+			'Authorization': 'Bearer ' + tokenData.access_token
 		}
 		return request(endpoint, {headers: headers});
 	}).then(function(body) {
 		var characterData = JSON.parse(body);
-		return req.app.get("characterModel").upsert(characterData)
+		return req.app.get('characterModel').upsert(characterData)
 	}).then(function(data) {
 		res.send(data);
 	}).catch(function(e) {
